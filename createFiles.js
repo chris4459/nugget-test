05edcda1dcbde1a9b7372a802f1a131bdced8100
Chris Lee <chrilee@factset.com>
2020-03-11 22:56:38 -0400

    more files

--- createFiles.js
+++ createFiles.js
@@ -4,7 +4,7 @@ fs.mkdir('./foo', { recursive: true }, (err) => {
   if (err) throw err;
 });
 
-for (var i = 0; i < 300; i++) {
+for (var i = 0; i < 3100; i++) {
   fs.writeFile(`./foo/a${i}.txt`, 'random text', (err) => {
     if (err) throw err;
   });
